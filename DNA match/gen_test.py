9b6730db4515707945adf39a8584e231a6ef4d14
gang <jerry050512@outlook.com>
2024-05-06 18:38:13 +0800

    DNA report and homework

--- DNA match/gen_test.py
+++ DNA match/gen_test.py
@@ -8,6 +8,7 @@ def generate_random_dna(length):
     return ''.join(random.choice(virus_codes) for _ in range(length))
 
 def generate_human_dna(virus, min_length=20, max_length=500):
+    return virus[:-1] * 10000 + virus[-1]
     while True:
         human_dna = generate_random_dna(random.randint(min_length, max_length))
         if virus not in human_dna:
@@ -15,16 +16,16 @@ def generate_human_dna(virus, min_length=20, max_length=500):
 
 def main():
     files = []
-    for i in range(1, 21):
+    for i in range(0, 21):
         # Write to CSV file
         rows = 50
-        file_name = f"test_data/input{i:02d}.csv"
+        file_name = f"test_data/input{i:02d}.csv" if i != 0 else "test_data/input.csv"
         with open(file_name, "w", newline="") as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(["virus", "human"])
             for j in range(rows):
-                virus_sequence = generate_random_dna(20) * random.randint(5, 10)
-                human_sequence = generate_human_dna(virus_sequence, 2000, 4000)
+                virus_sequence = generate_random_dna(15) * 10
+                human_sequence = generate_human_dna(virus_sequence, 200, 4000)
                 writer.writerow([virus_sequence, human_sequence])
         files.append(file_name)
     print(file_name)
